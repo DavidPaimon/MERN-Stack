941bfd252248672f5c1d5c33b6e202a1d42fe637
David Paimon <davidfelipe.fj@gmail.com>
2024-08-17 00:39:08 -0500

    Updated User / Profile

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -32,7 +32,6 @@ const createUser = asyncHandler(async (req, res) => {
         res.status(400)
         throw new Error("Invalid user data")
     }
-
 });
 
 const loginUser = asyncHandler(async (req, res) => {
@@ -59,11 +58,56 @@ const loginUser = asyncHandler(async (req, res) => {
 
 const logoutCurrentUser = asyncHandler(async (req, res) => {
     res.cookie('jwt', '', {
-        httyOnly: true,
+        httpOnly: true,
         expires: new Date(0),
     })
 
-    res.status(200).json({message: "Logged out successfully"})
+    res.status(200).json({message: "Logged out successfully"});
 });
 
-export { createUser, loginUser, logoutCurrentUser };
+const getAllUsers = asyncHandler(async (req, res) => {
+    const users = await User.find({});
+    res.json(users);
+});
+
+const getCurrentUserProfile = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.user._id)
+
+    if (user) {
+        res.json({
+            _id: user._id,
+            username: user.username,
+            email: user.email
+        })
+    } else {
+        res.status(404);
+        throw new Error("User not found");
+    }
+});
+
+const updateCurrentUserProfile = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.user._id)
+
+    if (user) {
+        user.username = req.body.username || user.username
+        user.email = req.body.email || user.email
+
+        if (req.body.password) {
+            user.password = req.body.password
+        }
+
+        const updatedUser = await user.save()
+
+        res.jason({
+            _id: updatedUser._id,
+            username: updatedUser.username,
+            email: updatedUser.email,
+            isAdmin: updatedUser.isAdmin
+        })
+    } else {
+        res.status(404)
+        throw new Error("User not found");
+    }
+});
+
+export { createUser, loginUser, logoutCurrentUser, getAllUsers, getCurrentUserProfile, updateCurrentUserProfile };
